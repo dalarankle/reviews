185e000c23174933205521734c8ba062239b9201
dalarankle <dalarankle@gmail.com>
2019-07-04 10:40:15 +0800

    new version

--- read.py
+++ read.py
@@ -6,7 +6,15 @@ with open('reviews.txt', 'r') as f:
         count += 1
         if count % 1000 == 0:
             print(len(data))
+print('檔案讀取完了，總共有',len(data) ,'筆資料')
 
-print(len(data))
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+print('留言的平均長度是', sum_len / len(data)) 
 
-
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆留言長度小於100字')
